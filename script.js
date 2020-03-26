5fb8dd7418a1b26d5120c072b583f42a668c3da0
Sarah Vieira <smarievieira@gmail.com>
2020-03-26 13:15:35 -0400

    Improved script.js functionality - added JQ hamburger menu

--- script.js
+++ script.js
@@ -1,55 +1,42 @@
 AOS.init();
 
-// Hamburger menu
-
-const menuBtn = document.querySelector('.menuBtn');
-const hamburgerImage = document.querySelector('.hamburgerImage');
-const exitImage = document.querySelector('.closeMenuImage');
-const menu = document.querySelector('.hamburgerMenu');
-
-menuBtn.addEventListener('click', () => {
-    // menu.classList.toggle('fadeOutUp')
-    
-    hamburgerImage.classList.toggle('open');
-    exitImage.classList.toggle('open');
-    menu.classList.toggle('open');
-});
-
-menu.addEventListener('click', () => {
-    hamburgerImage.classList.toggle('open');
-    exitImage.classList.toggle('open');
-    menu.classList.toggle('open');
-})
-
-// typed.js
-// https://forum.webflow.com/t/typed-js-timing/77944/4 - used to enable typing on scroll instead of refresh
-
-var typed = new Typed('.typed', {
-    strings: ["Will ask to pet your dog.", "Budget travel enthusiast.", "Probably eating ramen.", "Front-end developer."],
-    typeSpeed: 30,
-});
-
-/* stop type.js*/
-typed.stop();
-/* hide type.js cursoer */
-$(".typed-cursor").hide();
-/* dedect scroll into view */
-$(".typed").waypoint({
-    handler: function () {
-        typed.start();
-        $(".typed-cursor").show();
-    },
-    offset: '50%'
-});
-
-// Animate on scroll
-
-
-
-// -------------------------------------------
-
 $(document).ready(function () {
-    // app.init();
+
+    // Hamburger menu functionality
+    
+    $('.hamburgerImage').click(function(){
+        console.log("open menu");
+        $('.hamburgerMenu').show();
+        $('.closeMenuImage').show();
+        $('.hamburgerImage').hide();
+    })
+    
+    $('.closeMenuImage').click(function(){
+        console.log("close menu");
+        $('.hamburgerMenu').hide();
+        $('.closeMenuImage').hide();
+        $('.hamburgerImage').show();
+    })
+
 
     
+    // typed.js
+
+    var typed = new Typed('.typed', {
+        strings: ["Will ask to pet your dog.", "Budget travel enthusiast.", "Probably eating ramen.", "Front-end developer."],
+        typeSpeed: 30,
+    });
+    
+    /* stop type.js*/
+    typed.stop();
+    /* hide type.js cursoer */
+    $(".typed-cursor").hide();
+    /* dedect scroll into view */
+    $(".typed").waypoint({
+        handler: function () {
+            typed.start();
+            $(".typed-cursor").show();
+        },
+        offset: '50%'
+    });
 }); 
